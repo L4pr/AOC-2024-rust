594d60b4e3f83768639c76aebcd28c69631c4ebf
RenzoL4pr <556140@student.saxion.nl>
2024-12-07 23:47:35 +0100

    Added multithreading to part 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,5 +1,6 @@
 use std::collections::HashSet;
 use advent_of_code::make_grid;
+use std::thread;
 
 advent_of_code::solution!(6);
 
@@ -59,6 +60,70 @@ const DIRECTIONS: [(i32, i32); 4] = [
     (0, -1),
 ];
 
+
+// Single Threaded part 2 solution
+// pub fn part_two(input: &str) -> Option<u32> {
+//     let mut grid = make_grid(input);
+//     grid.push(vec!['?'; grid[0].len()]);
+//     grid.insert(0, vec!['?'; grid[0].len()]);
+//     for i in 0..grid.len() {
+//         grid[i].push('?');
+//         grid[i].insert(0, '?');
+//     }
+//     let mut start_location = (0, 0);
+//     for i in 0..grid.len() {
+//         for j in 0..grid.len() {
+//             if grid[i][j] == '^' {
+//                 start_location = (i, j);
+//                 break;
+//             }
+//         }
+//         if start_location.0 > 0 {
+//             break;
+//         }
+//     }
+//
+//
+//     let mut direction: usize = 0;
+//     let mut current_location = start_location;
+//     let mut visited: HashSet<(usize, usize)> = HashSet::new();
+//     visited.insert(current_location);
+//     while grid[current_location.0][current_location.1] != '?' {
+//         let next = move_in_direction(direction, &current_location);
+//         visited.insert(current_location);
+//         if grid[next.0][next.1] == '#' {
+//             direction = (direction + 1) % 4;
+//         } else {
+//             current_location = next;
+//         }
+//     }
+//     visited.remove(&current_location);
+//
+//     let mut result = 0;
+//     for location in visited {
+//         grid[location.0][location.1] = '#';
+//         let mut visited: HashSet<(usize, (usize, usize))> = HashSet::new();
+//         let mut current_location = start_location;
+//         let mut direction: usize = 0;
+//         while grid[current_location.0][current_location.1] != '?' {
+//             if visited.contains(&(direction, current_location)) {
+//                 result += 1;
+//                 break;
+//             }
+//             visited.insert((direction, current_location));
+//             let next = move_in_direction(direction, &current_location);
+//             if grid[next.0][next.1] == '#' {
+//                 direction = (direction + 1) % 4;
+//             } else {
+//                 current_location = next;
+//             }
+//         }
+//         grid[location.0][location.1] = '.';
+//     }
+//
+//     Some(result)
+// }
+
 pub fn part_two(input: &str) -> Option<u32> {
     let mut grid = make_grid(input);
     grid.push(vec!['?'; grid[0].len()]);
@@ -96,61 +161,53 @@ pub fn part_two(input: &str) -> Option<u32> {
     }
     visited.remove(&current_location);
 
-    let mut result = 0;
+    let visited_vec: Vec<_> = visited.into_iter().collect();
+    let chunk_size = visited_vec.len() / 12;
 
-    for location in visited {
-        if grid[location.0][location.1] != '.' {
-            continue;
-        }
-        grid[location.0][location.1] = '#';
-        let mut visited: HashSet<(usize, (usize, usize))> = HashSet::new();
-        let mut current_location = start_location;
-        let mut direction: usize = 0;
-        while grid[current_location.0][current_location.1] != '?' {
-            if visited.contains(&(direction, current_location)) {
-                result += 1;
-                break;
+    let chunks: Vec<_> = visited_vec.chunks(chunk_size).map(|chunk| chunk.to_vec()).collect();
+
+    let mut handles = Vec::new();
+
+    for chunk in chunks {
+        let mut grid_clone = grid.clone();
+
+        let handle = thread::spawn(move || {
+            let mut local_result = 0;
+            for location in chunk {
+                grid_clone[location.0][location.1] = '#';
+
+                let mut visited: HashSet<(usize, (usize, usize))> = HashSet::new();
+                let mut current_location = start_location;
+                let mut direction: usize = 0;
+
+                while grid_clone[current_location.0][current_location.1] != '?' {
+                    if visited.contains(&(direction, current_location)) {
+                        local_result += 1;
+                        break;
+                    }
+                    visited.insert((direction, current_location));
+                    let next = move_in_direction(direction, &current_location);
+                    if grid_clone[next.0][next.1] == '#' {
+                        direction = (direction + 1) % 4;
+                    } else {
+                        current_location = next;
+                    }
+                }
+
+                grid_clone[location.0][location.1] = '.';
             }
-            visited.insert((direction, current_location));
-            let next = move_in_direction(direction, &current_location);
-            if grid[next.0][next.1] == '#' {
-                direction = (direction + 1) % 4;
-            } else {
-                current_location = next;
-            }
-        }
-        grid[location.0][location.1] = '.';
+            local_result
+        });
+
+        handles.push(handle);
     }
 
+    let result: u32 = handles
+        .into_iter()
+        .map(|handle| handle.join().unwrap())
+        .sum();
 
-
-    // for i in 0..grid.len() {
-    //     for j in 0..grid.len() {
-    //         if grid[i][j] != '.' {
-    //             continue;
-    //         }
-    //         grid[i][j] = '#';
-    //         let mut visited: HashSet<(usize, (usize, usize))> = HashSet::new();
-    //         let mut current_location = start_location;
-    //         let mut direction: usize = 0;
-    //         while grid[current_location.0][current_location.1] != '?' {
-    //             if visited.contains(&(direction, current_location)) {
-    //                 result += 1;
-    //                 break;
-    //             }
-    //             visited.insert((direction, current_location));
-    //             let next = move_in_direction(direction, &current_location);
-    //             if grid[next.0][next.1] == '#' {
-    //                 direction = (direction + 1) % 4;
-    //             } else {
-    //                 current_location = next;
-    //             }
-    //         }
-    //         grid[i][j] = '.';
-    //     }
-    // }
-
-    Some(result as u32)
+    Some(result)
 }
 
 #[cfg(test)]
