98cffbaedabb1596898b1fe4f3bb26bb116f6eab
RenzoL4pr <556140@student.saxion.nl>
2024-12-14 02:24:33 +0100

    ran fmt on day 13

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -12,10 +12,66 @@ pub fn part_one(input: &str) -> Option<u32> {
         if index >= lines.len() {
             break;
         }
-        let button_a = lines[index].split("Button A: ").into_iter().last().unwrap().split(", ").into_iter().map(|x| x.split("+").into_iter().last().unwrap().parse::<i32>().unwrap()).collect::<Vec<i32>>();
-        let button_b = lines[index + 1].split("Button B: ").into_iter().last().unwrap().split(", ").into_iter().map(|x| x.split("+").into_iter().last().unwrap().parse::<i32>().unwrap()).collect::<Vec<i32>>();
-        let goal = lines[index + 2].split("Prize:  ").into_iter().last().unwrap().split(", ").into_iter().map(|x| x.split("=").into_iter().last().unwrap().parse::<i32>().unwrap()).collect::<Vec<i32>>();
-        let lowest_cost = find_prize(0, 0, button_a[0], button_a[1], button_b[0], button_b[1], 0, 0, goal[0], goal[1]);
+        let button_a = lines[index]
+            .split("Button A: ")
+            .into_iter()
+            .last()
+            .unwrap()
+            .split(", ")
+            .into_iter()
+            .map(|x| {
+                x.split("+")
+                    .into_iter()
+                    .last()
+                    .unwrap()
+                    .parse::<i32>()
+                    .unwrap()
+            })
+            .collect::<Vec<i32>>();
+        let button_b = lines[index + 1]
+            .split("Button B: ")
+            .into_iter()
+            .last()
+            .unwrap()
+            .split(", ")
+            .into_iter()
+            .map(|x| {
+                x.split("+")
+                    .into_iter()
+                    .last()
+                    .unwrap()
+                    .parse::<i32>()
+                    .unwrap()
+            })
+            .collect::<Vec<i32>>();
+        let goal = lines[index + 2]
+            .split("Prize:  ")
+            .into_iter()
+            .last()
+            .unwrap()
+            .split(", ")
+            .into_iter()
+            .map(|x| {
+                x.split("=")
+                    .into_iter()
+                    .last()
+                    .unwrap()
+                    .parse::<i32>()
+                    .unwrap()
+            })
+            .collect::<Vec<i32>>();
+        let lowest_cost = find_prize(
+            0,
+            0,
+            button_a[0],
+            button_a[1],
+            button_b[0],
+            button_b[1],
+            0,
+            0,
+            goal[0],
+            goal[1],
+        );
         if lowest_cost != -1 {
             result += lowest_cost;
         }
@@ -25,7 +81,18 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(result as u32)
 }
 
-fn find_prize(x: i32, y: i32, ax: i32, ay: i32, bx: i32, by: i32, a_pressed: u32, b_pressed: u32, goal_x: i32, goal_y: i32) -> i32 {
+fn find_prize(
+    x: i32,
+    y: i32,
+    ax: i32,
+    ay: i32,
+    bx: i32,
+    by: i32,
+    a_pressed: u32,
+    b_pressed: u32,
+    goal_x: i32,
+    goal_y: i32,
+) -> i32 {
     if x == goal_x && y == goal_y {
         return 0;
     }
@@ -46,7 +113,18 @@ fn find_prize(x: i32, y: i32, ax: i32, ay: i32, bx: i32, by: i32, a_pressed: u32
     }
     let mut newxa = -1;
     if b_pressed < 100 {
-        newxa = find_prize(x + ax, y + ay, ax, ay, bx, by, a_pressed + 1, b_pressed, goal_x, goal_y);
+        newxa = find_prize(
+            x + ax,
+            y + ay,
+            ax,
+            ay,
+            bx,
+            by,
+            a_pressed + 1,
+            b_pressed,
+            goal_x,
+            goal_y,
+        );
     }
     if newxa != -1 {
         if b_amount != -1 {
@@ -73,7 +151,6 @@ fn find_prize_part2(ax: i64, ay: i64, bx: i64, by: i64, goal_x: i64, goal_y: i64
     let det_da = goal_x * by - goal_y * bx;
     let det_db = ax * goal_y - ay * goal_x;
 
-
     if det_da % det_d != 0 || det_db % det_d != 0 {
         return -1;
     }
@@ -98,10 +175,62 @@ pub fn part_two(input: &str) -> Option<u64> {
         if index >= lines.len() {
             break;
         }
-        let button_a = lines[index].split("Button A: ").into_iter().last().unwrap().split(", ").into_iter().map(|x| x.split("+").into_iter().last().unwrap().parse::<i64>().unwrap()).collect::<Vec<i64>>();
-        let button_b = lines[index + 1].split("Button B: ").into_iter().last().unwrap().split(", ").into_iter().map(|x| x.split("+").into_iter().last().unwrap().parse::<i64>().unwrap()).collect::<Vec<i64>>();
-        let goal = lines[index + 2].split("Prize:  ").into_iter().last().unwrap().split(", ").into_iter().map(|x| x.split("=").into_iter().last().unwrap().parse::<i64>().unwrap()).collect::<Vec<i64>>();
-        let lowest_cost = find_prize_part2(button_a[0], button_a[1], button_b[0], button_b[1], goal[0] + 10000000000000, goal[1] + 10000000000000);
+        let button_a = lines[index]
+            .split("Button A: ")
+            .into_iter()
+            .last()
+            .unwrap()
+            .split(", ")
+            .into_iter()
+            .map(|x| {
+                x.split("+")
+                    .into_iter()
+                    .last()
+                    .unwrap()
+                    .parse::<i64>()
+                    .unwrap()
+            })
+            .collect::<Vec<i64>>();
+        let button_b = lines[index + 1]
+            .split("Button B: ")
+            .into_iter()
+            .last()
+            .unwrap()
+            .split(", ")
+            .into_iter()
+            .map(|x| {
+                x.split("+")
+                    .into_iter()
+                    .last()
+                    .unwrap()
+                    .parse::<i64>()
+                    .unwrap()
+            })
+            .collect::<Vec<i64>>();
+        let goal = lines[index + 2]
+            .split("Prize:  ")
+            .into_iter()
+            .last()
+            .unwrap()
+            .split(", ")
+            .into_iter()
+            .map(|x| {
+                x.split("=")
+                    .into_iter()
+                    .last()
+                    .unwrap()
+                    .parse::<i64>()
+                    .unwrap()
+            })
+            .collect::<Vec<i64>>();
+        let lowest_cost = find_prize_part2(
+            button_a[0],
+            button_a[1],
+            button_b[0],
+            button_b[1],
+            goal[0] + 10000000000000,
+            goal[1] + 10000000000000,
+        );
         if lowest_cost != -1 {
             result += lowest_cost;
         }
